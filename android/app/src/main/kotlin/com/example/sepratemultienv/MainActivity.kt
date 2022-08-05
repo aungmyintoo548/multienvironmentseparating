7f12f86541898188fa56e1e8cf9c7d1f257cd802
tmn <thantmonnaing@cloudsource.co.jp>
2022-08-05 11:23:29 +0630

    set up ios and android env

--- android/app/src/main/kotlin/com/example/sepratemultienv/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sepratemultienv/MainActivity.kt
@@ -1,6 +1,20 @@
-package com.example.sepratemultienv
+package com.amo.authtest
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import androidx.annotation.NonNull
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "flavor"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method.equals("getFlavor")) {
+                result.success(BuildConfig.FLAVOR);
+            }
+        }
+    }
 }
